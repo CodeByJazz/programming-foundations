e7a6e56ac158fbadea0cc19acf827c0860980483
Jazz Whitfield <jazzwhitfield123@gmail.com>
2022-09-28 14:01:33 -0400

    Upload all created files.

---
+++ lesson_2/pseudo-code.rb
@@ -0,0 +1,77 @@
+#1. a method that returns the sum of two integers
+
+# Ask the user for two numbers
+
+# Store each number into a variable
+
+# define a method that takes two parameters
+
+# create a variable called result within the method 
+
+# assign it to the expression num1 + num2 
+
+# output the result to the user
+
+#2. 
+
+#START
+# PRINT "What is your first number?"
+# GET the first number 
+# SET num1 to the first number
+
+# PRINT "What is your second number?"
+# GET the second number 
+# SET num2 to the second number
+
+# DEF a method called sum with two parameters (num1, num2)
+# SET result to num1 + num
+
+#PRINT result
+
+#END
+
+#2. a method that takes an array of strings, and returns a string that is all those strings concatenated together
+
+# Define a method called "concat" that takes one parameter called "array"
+
+# Join each item of the array together to form one single array
+
+# Convert the new array to a string
+
+# Output the results 
+
+#START 
+
+#DEF a method called concat with one parameter (array)
+
+#SET new_array to equal array.join(" ")
+
+#SET new_array to new_array.to_s()
+
+#PRINT new_array
+
+#END
+
+
+#3. a method that takes an array of integers, and returns a new array with every other element from the orignal array, starting with the first element
+
+# Define a method called "everyOther" that accepts one parameter called "array"
+
+# Output the first element of the array 
+
+# Create a variable called new_array assign it to the return value of every other integer from the argument 
+
+# Output new_array 
+
+
+#START 
+
+# DEF a method called everyOther with one parameter (array)
+
+#PRINT the first element of the array
+
+#SET new_array to equal the return value of every other integer from the original array 
+
+#PRINT new_array 
+
+#END
