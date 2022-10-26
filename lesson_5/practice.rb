d050732d1f84a7e7735ecb755d62bb867c786a00
Jazz Whitfield <jazzwhitfield123@gmail.com>
2022-10-26 14:01:35 -0400

    Update practice file in lesson 5.

--- lesson_5/practice.rb
+++ lesson_5/practice.rb
@@ -216,27 +216,3 @@
 
 #   uuid
 # end
-todo_lists = [
-  {
-    id: 1,
-    list_name: 'Groceries',
-    todos: [
-      { id: 1, name: 'Bread', completed: false },
-      { id: 2, name: 'Milk', completed: false },
-      { id: 3, name: 'Apple Juice', completed: false }
-    ]
-  },
-  {
-    id: 2,
-    list_name: 'Homework',
-    todos: [
-      { id: 1, name: 'Math', completed: false },
-      { id: 2, name: 'English', completed: false }
-    ]
-  }
-]
-
-todo_lists[1][:todos][0][:completed] = true
-todo_lists[1][:todos][1][:completed] = true
-
-p todo_lists
