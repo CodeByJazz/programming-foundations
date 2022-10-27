a6ce1b647967316543aed12d9d5f7487a64bc355
Jazz Whitfield <jazzwhitfield123@gmail.com>
2022-10-27 17:19:04 -0400

    Create constants for the card suits and values. Create a method that determines the value of the ace depending on the value of the second card.

---
+++ lesson_6/twenty_one.rb
@@ -0,0 +1,40 @@
+SUITS = ['H', 'D', 'S', 'C']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def total(cards)
+  values = cards.map { |card| card[1] }
+
+  sum = 0
+  values.each do |value|
+    if value == "A"
+      sum += 11
+    elsif value.to_i == 0 # J, Q, K
+      sum += 10
+    else
+      sum += value.to_i
+    end
+  end
+
+  values.select { |value| value == "A" }.count.times do 
+    sum -= 10 if sum > 21
+  end
+
+  sum
+end
+
+answer = nil
+loop do 
+  puts "hit or stay?"
+  answer = gets.chomp
+  break if answer == 'stay' || busted?
+end
+
+if busted? 
+
+else
+  puts "You chose to stay!"
+end 
